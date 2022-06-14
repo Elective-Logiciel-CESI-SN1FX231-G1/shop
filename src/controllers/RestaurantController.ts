55c39a6dc70e48989d23b081ac896e3048fec2fd
OlivierS7 <olivier.simonneau@viacesi.fr>
2022-06-14 16:39:41 +0200

    Create RestaurantController

---
+++ src/controllers/RestaurantController.ts
@@ -0,0 +1,29 @@
+import { Handler } from 'express'
+
+export const create: Handler = async (req, res) => {
+
+}
+
+export const getAll: Handler = async (req, res) => {
+
+}
+
+export const getOne: Handler = async (req, res) => {
+
+}
+
+export const modify: Handler = async (req, res) => {
+
+}
+
+export const remove: Handler = async (req, res) => {
+
+}
+
+export default {
+  create,
+  getAll,
+  getOne,
+  modify,
+  remove
+}
