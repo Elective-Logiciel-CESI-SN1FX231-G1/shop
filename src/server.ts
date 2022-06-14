76045151ac72754ead5d6ce3742d41520d3d4bc2
TomBANCHEREAU <tom.banchereau@viacesi.fr>
2022-06-14 15:31:34 +0200

    setting up mqtt

--- src/server.ts
+++ src/server.ts
@@ -2,12 +2,15 @@ import type { Server } from 'http'
 import mongoose from 'mongoose'
 import config from 'config'
 import app from './app'
-
+import { connect } from './mqtt'
 let server: Server
 
 export default {
   start: async () => {
     await mongoose.connect(config.get('mongo.url'))
+    console.log('Connected to mongodb')
+    await connect()
+    console.log('Connected to mqtt')
     server = app.listen(config.get('http.port'))
     await new Promise(resolve => server.once('listening', resolve))
     console.log(`Http server listening on: ${config.get('http.publicUrl')}`)
