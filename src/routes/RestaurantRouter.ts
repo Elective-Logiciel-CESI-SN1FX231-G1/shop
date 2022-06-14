85fb6eb7e568b4a20e519bb9a10596b8a8317339
OlivierS7 <olivier.simonneau@viacesi.fr>
2022-06-14 16:40:11 +0200

    Create RestaurantRouter

---
+++ src/routes/RestaurantRouter.ts
@@ -0,0 +1,15 @@
+import express from 'express'
+import restaurantController from '../controllers/RestaurantController'
+const restaurantRouter = express.Router()
+
+restaurantRouter.post('/', express.json(), restaurantController.create)
+
+restaurantRouter.get('/', restaurantController.getAll)
+
+restaurantRouter.get('/:id', restaurantController.getOne)
+
+restaurantRouter.patch('/:id', restaurantController.modify)
+
+restaurantRouter.delete('/:id', restaurantController.remove)
+
+export default restaurantRouter
