9ecb491b6b4cbf8a9ff37a0788e36fc95370b557
OlivierS7 <olivier.simonneau@viacesi.fr>
2022-06-14 16:39:55 +0200

    Create RestaurantModel

---
+++ src/models/RestaurantModel.ts
@@ -0,0 +1,48 @@
+import { Schema, model } from 'mongoose'
+
+interface IRestaurant {
+  owner: {
+    firstName: string,
+    lastName: string,
+    phone: string,
+    email: string
+  },
+  name: string,
+  description: string,
+  address: string,
+  position:{
+    lon:number,
+    lat:number
+  },
+  openingHours:[
+    {from:Date, to:Date}
+  ],
+  types: Array<string>,
+  isClosed: boolean
+}
+
+const RestaurantSchema = new Schema<IRestaurant>({
+  owner: {
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    phone: { type: String, required: true },
+    email: { type: String, required: true, unique: true }
+  },
+  name: { type: String, required: true },
+  description: { type: String },
+  address: { type: String, required: true },
+  position: {
+    lon: { type: Number, required: true },
+    lat: { type: Number, required: true }
+  },
+  openingHours: [
+    {
+      from: { type: Date, required: true },
+      to: { type: Date, required: true }
+    }
+  ],
+  types: [{ type: String, required: true }],
+  isClosed: { type: Boolean, required: true }
+})
+
+export default model('Restaurant', RestaurantSchema)
