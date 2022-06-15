7a3fab9ca732788c037129bd384367fc0931eb10
OlivierS7 <olivier.simonneau@viacesi.fr>
2022-06-15 09:46:01 +0200

    Handle error messages for Restaurant API calls

--- src/controllers/RestaurantController.ts
+++ src/controllers/RestaurantController.ts
@@ -5,46 +5,37 @@ export const create: Handler = async (req, res) => {
   const Restaurant = new RestaurantModel(req.body)
   try {
     await Restaurant.save()
-    res.send(Restaurant)
+    res.status(201).send(Restaurant)
   } catch (err) {
-    res.status(400).json(err)
+    if (err instanceof Error && err.message) res.status(400).send(err.message)
+    else throw err
   }
 }
 
 export const getAll: Handler = async (req, res) => {
-  try {
-    const Restaurants = await RestaurantModel.find()
-    res.send(Restaurants)
-  } catch (err) {
-    res.status(400).json(err)
-  }
+  const Restaurants = await RestaurantModel.find()
+  res.send(Restaurants)
 }
 
 export const getOne: Handler = async (req, res) => {
-  try {
-    const Restaurant = await RestaurantModel.findOne({ _id: req.params.id })
-    res.send(Restaurant)
-  } catch (err) {
-    res.status(400).json(err)
-  }
+  const Restaurant = await RestaurantModel.findOne({ _id: req.params.id })
+  Restaurant ? res.send(Restaurant) : res.status(404).send('Restaurant Not Found')
 }
 
 export const modify: Handler = async (req, res) => {
   try {
-    const Restaurant = await RestaurantModel.findOneAndUpdate({ _id: req.params.id }, req.body, { new: true })
-    res.send(Restaurant)
+    const Restaurant = await RestaurantModel.findOneAndUpdate({ _id: req.params.id }, { $set: req.body }, { new: true }).exec()
+    if (Restaurant) res.send(Restaurant)
+    else res.status(404).send('Restaurant Not Found')
   } catch (err) {
-    res.status(400).json(err)
+    if (err instanceof Error && err.message) res.status(400).send(err.message)
+    else throw err
   }
 }
 
 export const remove: Handler = async (req, res) => {
-  try {
-    const Restaurant = await RestaurantModel.deleteOne({ _id: req.params.id })
-    res.send(Restaurant)
-  } catch (err) {
-    res.status(400).json(err)
-  }
+  const Restaurant = await RestaurantModel.deleteOne({ _id: req.params.id })
+  Restaurant.deletedCount ? res.sendStatus(204) : res.status(404).send('Restaurant Not Found')
 }
 
 export default {
