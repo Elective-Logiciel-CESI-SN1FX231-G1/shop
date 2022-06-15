5815bb2e35a89f68fc9afbbdfb722f3d59385575
OlivierS7 <olivier.simonneau@viacesi.fr>
2022-06-15 09:51:36 +0200

    Handle error messages for Product API calls

--- src/controllers/ProductController.ts
+++ src/controllers/ProductController.ts
@@ -5,46 +5,39 @@ export const create: Handler = async (req, res) => {
   const Product = new ProductModel(req.body)
   try {
     await Product.save()
-    res.send(Product)
+    res.status(201).send(Product)
   } catch (err) {
-    res.status(400).json(err)
+    if (err instanceof Error && err.message) res.status(400).send(err.message)
+    else throw err
   }
 }
 
 export const getAll: Handler = async (req, res) => {
-  try {
-    const Products = await ProductModel.find()
-    res.send(Products)
-  } catch (err) {
-    res.status(400).json(err)
-  }
+  const Products = await ProductModel.find()
+  res.send(Products)
 }
 
 export const getOne: Handler = async (req, res) => {
-  try {
-    const Product = await ProductModel.findOne({ _id: req.params.id })
-    res.send(Product)
-  } catch (err) {
-    res.status(400).json(err)
-  }
+  const Product = await ProductModel.findOne({ _id: req.params.id })
+  if (Product) res.send(Product)
+  else res.status(404).send('Product Not Found')
 }
 
 export const modify: Handler = async (req, res) => {
   try {
-    const Product = await ProductModel.findOneAndUpdate({ _id: req.params.id }, req.body, { new: true })
-    res.send(Product)
+    const Product = await ProductModel.findOneAndUpdate({ _id: req.params.id }, { $set: req.body }, { new: true })
+    if (Product) res.send(Product)
+    else res.status(404).send('Product Not Found')
   } catch (err) {
-    res.status(400).json(err)
+    if (err instanceof Error && err.message) res.status(400).send(err.message)
+    else throw err
   }
 }
 
 export const remove: Handler = async (req, res) => {
-  try {
-    const Product = await ProductModel.deleteOne({ _id: req.params.id })
-    res.send(Product)
-  } catch (err) {
-    res.status(400).json(err)
-  }
+  const Product = await ProductModel.deleteOne({ _id: req.params.id })
+  if (Product.deletedCount) res.send(Product)
+  else res.status(404).send('Product Not found')
 }
 
 export default {
