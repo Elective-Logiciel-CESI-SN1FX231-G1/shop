5ad7f3711286d914a2470811d767e6cbcb268b10
TomBANCHEREAU <tom.banchereau@viacesi.fr>
2022-06-16 12:42:30 +0200

    authenticate users

--- src/app.ts
+++ src/app.ts
@@ -5,9 +5,11 @@ import ProductRouter from './routes/ProductRouter'
 import RestaurantRouter from './routes/RestaurantRouter'
 import MenuRouter from './routes/MenuRouter'
 import OrderRouter from './routes/OrderRouter'
+import { auth } from './auth'
 
 const app = express()
 
+app.use(auth)
 app.use('/api/restaurants', RestaurantRouter)
 app.use('/api/products', ProductRouter)
 app.use('/api/menus', MenuRouter)
