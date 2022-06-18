07afa49d014520cea521adad13f80a280b66d73e
OlivierS7 <olivier.simonneau@viacesi.fr>
2022-06-18 11:46:35 +0200

    Adding a new topic

--- src/mqtt.ts
+++ src/mqtt.ts
@@ -1,13 +1,14 @@
 import mqtt from 'async-mqtt'
 import config from 'config'
-import { processSponsorRestaurateur } from './controllers/CouponController'
+import { processSponsor } from './controllers/CouponController'
 
 const client = mqtt.connect(config.get('mqtt.url'))
 
 client.on('message', async function (topic, message) {
   try {
     const msg = JSON.parse(message.toString())
-    if (topic === 'sponsor/sponsorship/restaurateur') await processSponsorRestaurateur(msg)
+    if (topic === 'sponsor/sponsorship/restaurateur') await processSponsor(msg)
+    if (topic === 'sponsor/sponsorship/client') await processSponsor(msg)
   } catch (error) {
     console.error(error)
   }
@@ -16,6 +17,7 @@ client.on('message', async function (topic, message) {
 export const connect = async function () {
   if (!client.connected) { await new Promise(resolve => client.once('connect', resolve)) }
   await client.subscribe('sponsor/sponsorship/restaurateur')
+  await client.subscribe('sponsor/sponsorship/client')
 }
 
 export default client
